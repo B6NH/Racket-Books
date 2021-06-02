655da13a657c3cdb124f5c22fef1c84795aa5644
Jan Kowalski <yakov85@o2.pl>
2021-06-03 00:10:49 +0200

    Array

--- Simply Scheme/Part VI/Chapter 23/ex15.rkt
+++ Simply Scheme/Part VI/Chapter 23/ex15.rkt
@@ -2,4 +2,54 @@
 
 #lang racket
 
-#t
+(define (fill-helper vec size index)
+  (if (= index -1)
+      vec
+      (begin
+        (vector-set! vec index (make-vector size))
+        (fill-helper vec size (- index 1)))))
+
+(define (fill-vector vec size)
+  (fill-helper vec size (- (vector-length vec) 1)))
+
+(define (fill-vectors-helper vec lst index)
+  (if (= -1 index)
+      vec
+      (begin
+        (helper (vector-ref vec index) lst)
+        (fill-vectors-helper vec lst (- index 1)))))
+
+(define (fill-vectors vec lst)
+  (fill-vectors-helper vec lst (- (vector-length vec) 1)))
+
+(define (helper vec lst)
+  (begin
+    (fill-vector vec (car lst))
+    (let ((rst (cdr lst)))
+      (if (empty? rst)
+          vec
+          (fill-vectors vec rst)))))
+
+(define (make-array lst)
+  (let ((new (make-vector (car lst)))
+        (rst (cdr lst)))
+    (if (empty? rst)
+        new
+        (helper new rst))))
+
+(define (array-set! arr indices value)
+  (if (empty? (cdr indices))
+      (vector-set! arr (car indices) value)
+      (array-set! (vector-ref arr (car indices)) (cdr indices) value)))
+
+(define (array-ref arr indices)
+  (if (empty? (cdr indices))
+      (vector-ref arr (car indices))
+      (array-ref (vector-ref arr (car indices)) (cdr indices))))
+
+
+(define a1 (make-array '(4 5 6)))
+
+(array-set! a1 '(3 2 3) '(the end))
+(equal? (array-ref a1 '(3 2 3)) '(the end))
+
--- Simply Scheme/Part VI/Chapter 23/ex16.rkt
+++ Simply Scheme/Part VI/Chapter 23/ex16.rkt
@@ -2,4 +2,109 @@
 
 #lang racket
 
-#t
+(define (copy-list-from lst vec index)
+  (if (empty? lst)
+      index
+      (begin
+        (vector-set! vec index (car lst))
+        (copy-list-from (cdr lst) vec (+ index 1)))))
+
+(define (copy-element-from el vec index)
+  (begin
+    (vector-set! vec index el)
+    (+ index 1)))
+
+(define (list-to-vec lst vec index)
+  (if (empty? lst)
+      vec
+      (let ((fst (car lst)))
+        (list-to-vec
+          (cdr lst)
+          vec
+          ((if (list? fst) copy-list-from copy-element-from)
+           fst vec index)))))
+
+(define (v-helper source target index offset)
+  (if (= index (vector-length target))
+      target
+      (begin
+        (vector-set! target index (vector-ref source (+ index offset)))
+        (v-helper source target (+ index 1) offset))))
+
+(define (real-length-helper args acc)
+  (if (empty? args)
+      acc
+      (let ((fst (car args)))
+        (real-length-helper
+          (cdr args) (+ acc (if (list? fst) (length fst) 1))))))
+
+(define (real-length args)
+  (real-length-helper args 0))
+
+(define (sentence . args)
+  (list-to-vec args (make-vector (real-length args)) 0))
+
+(define (vempty? sent)
+  (zero? (vector-length sent)))
+
+(define (vfirst sent)
+  (vector-ref sent 0))
+
+(define (v-but sent offset)
+  (v-helper sent (make-vector (- (vector-length sent) 1)) 0 offset))
+
+(define (vbutfirst sent)
+  (v-but sent 1))
+
+(define (vbutlast sent)
+  (v-but sent 0))
+
+(define (vlast sent)
+  (vector-ref sent (- (vector-length sent) 1)))
+
+(define (praise stuff)
+  (sentence stuff '(is good)))
+
+(define (praise2 stuff)
+  (sentence stuff 'rules!))
+
+; Slower
+(define (item n sent)
+  (if (= n 1)
+      (vfirst sent)
+      (item (- n 1) (vbutfirst sent))))
+
+; Faster
+(define (item-v2 n sent)
+  (vector-ref sent (- n 1)))
+
+(define (vecons el vec)
+  (let ((new (make-vector (+ (vector-length vec) 1))))
+    (begin
+      (vector-set! new 0 el)
+      (v-helper vec new 1 -1))))
+
+(define (every fn sent)
+  (if (vempty? sent)
+      sent
+      (vecons (fn (vfirst sent))
+              (every fn (vbutfirst sent)))))
+
+
+(and
+  (equal? (vempty? (sentence)) #t)
+  (equal? (vempty? (sentence 'a 'b 'c)) #f)
+  (equal? (vfirst (sentence 'a 'b 'c)) 'a)
+  (equal? (vlast (sentence 'a 'b 'c)) 'c)
+  (equal? (vbutfirst (sentence 'a 'b 'c)) #(b c))
+  (equal? (vbutlast (sentence 'a 'b 'c)) #(a b))
+  (equal? (real-length '(water (fire air))) 3)
+  (equal? (praise 'water) #(water is good))
+  (equal? (praise '(water fire)) #(water fire is good))
+  (equal? (praise2 'air) #(air rules!))
+  (equal? (praise2 '(hello world)) #(hello world rules!))
+  (equal? (item 1 #(music book air)) 'music)
+  (equal? (item-v2 1 #(music book air)) 'music)
+  (equal? (item 3 #(fish tiger flower)) 'flower)
+  (equal? (item-v2 3 #(fish tiger flower)) 'flower)
+  (equal? (every (lambda(x)(* x 2)) #(5 6 7)) #(10 12 14)))
