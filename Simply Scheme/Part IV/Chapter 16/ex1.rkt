a05303ab3e0efed7e9f241872e8d4b3201825f2f
Jan Kowalski <yakov85@o2.pl>
2022-01-17 22:29:02 +0100

    Pattern Matcher

--- Simply Scheme/Part IV/Chapter 16/ex1.rkt
+++ Simply Scheme/Part IV/Chapter 16/ex1.rkt
@@ -3,11 +3,17 @@
 #lang racket
 (require (planet dyoo/simply-scheme:2:2))
 
+;; Check if sentence matches the pattern
 (define (match pattern sent)
+
+  ;; Database is initially empty
   (match-using-known-values pattern sent '()))
 
+;; Match with database values
 (define (match-using-known-values pattern sent known-values)
   (cond
+
+    ;; If the pattern is empty, the sentence should also be empty
     ((empty? pattern)
      (if (empty? sent) known-values 'failed))
     ((special? (first pattern))
@@ -23,6 +29,7 @@
       (bf pattern) (bf sent) known-values))
     (else 'failed)))
 
+;; Check special symbol
 (define (special? wd)
   (member? (first wd) '(* & ? !)))
 
@@ -98,6 +105,7 @@
 
 ;;; Known values database abstract data type
 
+;; Get value from database
 (define (lookup name known-values)
   (cond
     ((empty? known-values) 'no-value)
@@ -105,22 +113,29 @@
      (get-value (bf known-values)))
     (else (lookup name (skip-value known-values)))))
 
+;; Get value from the dictionary up to the symbol '!'
 (define (get-value stuff)
   (if (equal? (first stuff) '!)
       '()
       (se (first stuff) (get-value (bf stuff)))))
 
+;; Skip value from the dictionary up to the symbol '!'
 (define (skip-value stuff)
   (if (equal? (first stuff) '!)
       (bf stuff)
       (skip-value (bf stuff))))
 
+;; Add value to dictionary
 (define (add name value known-values)
   (if (empty? name)
       known-values
       (se known-values name value '!)))
 
 ; ------------------------------------------------------
+
+;; Exercises about Using the Pattern Matcher
+
+(displayln "***Exercises***")
 
 ; Ex 1
 
@@ -190,6 +205,26 @@
   (equal? (match p3 '(a b a b a a)) '(x a ! y b a !))
   (equal? (match p3 '(a b a a a b)) '(x a b ! y a !)))
 
+; ------------------------------------------------------
 
+;; Tests
 
+(displayln "***Tests***")
+
+(define db1 '(x mega drive ! y water !))
+
+(and
+
+  ;; Lookup
+  (equal? (lookup 'x db1) '(mega drive))
+  (equal? (lookup 'y db1) '(water))
+  (equal? (lookup 'z db1) 'no-value)
+
+  ;; Get & Skip
+  (equal? (get-value db1) '(x mega drive))
+  (equal? (skip-value db1) '(y water !))
+
+  ;; Add
+  (equal? (add 'neuroframe '(the wrath of code) db1)
+         '(x mega drive ! y water ! neuroframe the wrath of code !)))
 
