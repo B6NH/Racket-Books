05864dc0cdc4ebfbf17ed027adf1375267f14cb8
Jan Kowalski <yakov85@o2.pl>
2022-07-26 22:13:01 +0200

    Amb Kalotan Colors

--- Teach Yourself Scheme in Fixnum Days/helper/functions.scm
+++ Teach Yourself Scheme in Fixnum Days/helper/functions.scm
@@ -15,3 +15,25 @@
        ((eqv? (car l) o) i)
        (else (loop (+ i 1) (cdr l)))))))
 
+(define reverse!
+  (lambda (s)
+
+    ; Initially, r is empty list
+    (let loop ((s s) (r '()))
+
+      ; Return inverted list
+      (if (null? s)
+          r
+
+          ; Save value of (cdr s), which in next iteration will become new s
+          (let ((d (cdr s)))
+
+            ; Variable s becomes new r with old r as cdr
+            ; '(1 2 3) '() -> '(1),
+            ; '(2 3) '(1) -> '(2 1),
+            ; '(3) '(2 1) -> '(3 2 1)
+            (set-cdr! s r)
+
+            ; Loop with d as new s and s as new r
+            (loop d s))))))
+
--- Teach Yourself Scheme in Fixnum Days/nondeterminism.scm
+++ Teach Yourself Scheme in Fixnum Days/nondeterminism.scm
@@ -1,6 +1,8 @@
+(include "helper/functions.scm")
 
 ; Nondeterminism
 
+; Variable in which continuation will be stored
 (define amb-fail '*)
 
 (define initialize-amb-fail
@@ -9,24 +11,34 @@
       (lambda ()
         (error "amb tree exhausted")))))
 
-(initialize-amb-fail)
-
 (define-macro amb
+
+  ; Indefinite number of subexpressions
   (lambda alts...
+
+    ; Store current amb-fail value
     `(let ((+prev-amb-fail amb-fail))
        (call/cc
         (lambda (+sk)
 
+          ; Try all subexpressions
           ,@(map (lambda (alt)
                    `(call/cc
                      (lambda (+fk)
                        (set! amb-fail
                          (lambda ()
+
+                           ; Reset amb-fail
                            (set! amb-fail +prev-amb-fail)
+
+                           ; Try next alternate
                            (+fk 'fail)))
+
+                       ; Exit amb call
                        (+sk ,alt))))
                  alts...)
 
+          ; All alternates fail
           (+prev-amb-fail))))))
 
 (define number-between
@@ -36,9 +48,35 @@
           (amb)
           (amb i (loop (+ i 1)))))))
 
+; Amb fail if pred is false
 (define assert
   (lambda (pred)
     (if (not pred) (amb))))
+
+(define (distinct? lst)
+  (not (eqv? (car lst) (cadr lst))))
+
+(define (xor a b)
+  (or (and (not a) b)
+      (and a (not b))))
+
+(define (prime? n)
+  (letrec
+    ((h (lambda (c a)
+           (or (= 1 c)
+               (and (not (zero? (remainder a c)))
+                    (h (- c 1) a))))))
+    (h (- n 1) n)))
+
+(define (all-true pred lst)
+  (or (null? lst)
+      (and (pred (car lst))
+           (all-true pred (cdr lst)))))
+
+(define (all-false pred lst)
+  (or (null? lst)
+      (and (not (pred (car lst)))
+           (all-false pred (cdr lst)))))
 
 (define gen-prime
   (lambda (hi)
@@ -54,6 +92,8 @@
              (lambda (+k)
                (set! amb-fail (lambda () (+k #f)))
                (let ((+v ,e))
+
+                 ; Add next value
                  (set! +results (cons +v +results))
                  (+k #t))))
            (amb-fail))
@@ -79,11 +119,17 @@
           (kibi (amb 'm 'f))
           (kibi-self-desc (amb 'm 'f))
           (kibi-lied? (amb #t #f)))
+
+      ; Heterosexual couple
       (assert
        (distinct? (list parent1 parent2)))
-      (assert
-       (if (eqv? kibi 'm)
-           (not kibi-lied?)))
+
+      ; Error?
+;;       (assert
+;;        (if (eqv? kibi 'm)
+;;            (not kibi-lied?)))
+
+      ; Kibi lies
       (assert
        (if kibi-lied?
            (xor
@@ -91,6 +137,8 @@
                  (eqv? kibi 'f))
             (and (eqv? kibi-self-desc 'f)
                  (eqv? kibi 'm)))))
+
+      ; Kibi tells the truth
       (assert
        (if (not kibi-lied?)
            (xor
@@ -98,20 +146,33 @@
                  (eqv? kibi 'm))
             (and (eqv? kibi-self-desc 'f)
                  (eqv? kibi 'f)))))
+
+      ; Male - parent1, Female - parent2
       (assert
        (if (eqv? parent1 'm)
            (and
+
+            ; One true statement
+            ; Kibi said he is a boy
             (eqv? kibi-self-desc 'm)
+
+            ; One true and one false statement
+            ; Kibi is a girl and doesn't lie or is a boy and lies
             (xor
              (and (eqv? kibi 'f)
                   (eqv? kibi-lied? #f))
              (and (eqv? kibi 'm)
                   (eqv? kibi-lied? #t))))))
+
+      ; Female - parent1, Male - parent2
       (assert
        (if (eqv? parent1 'f)
+
+           ; Two true statements
            (and
             (eqv? kibi 'f)
             (eqv? kibi-lied? #t))))
+
       (list parent1 parent2 kibi))))
 
 ; -------------------------------------------------------------
@@ -131,8 +192,7 @@
           (l (choose-color)) ; Luxemb
           (i (choose-color)) ; Italy
           (s (choose-color)) ; Switz
-          (a (choose-color)) ; Austria
-          )
+          (a (choose-color))) ; Austria
 
       ; construct the adjacency list for
       ; each country: the 1st element is
@@ -202,15 +262,26 @@
 
 ; -------------------------------------------------------------
 
-(line)
+(define prime '(2 3 5 7 11 13 17 19 23 29))
+(define composite '(4 6 8 9 10 12 14 15 16 18))
 
-(display "Colors of countries:\n")
+(begin
 
-(color-europe)
+  (initialize-amb-fail)
 
-(line)
+  (line)
 
-(display
-  (= (amb 1 2 3 4 5 6 7 8 9 10) 1))
-(newline)
+  (display "Colors of countries:\n")
+  (color-europe)
+
+  (line)
+
+  (display
+    (and (= (amb 1 2 3 4 5 6 7 8 9 10) 1)
+         (all-true prime? prime)
+         (all-false prime? composite)
+         (equal? (bag-of (gen-prime 30)) prime)
+         (equal? (solve-kalotan-puzzle) '(f m f))))
+
+  (newline))
 
